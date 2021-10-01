280bde4387c7f331cfcbb8294aec828250309690
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-10-01 18:18:02 -0400

    adds functionality and style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+class Contact extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        password: 'swordfish',
+        authorized: false
+      };
+      this.authorize = this.authorize.bind(this);
+  }
+  
+    authorize = (e) => {
+      const password = e.target.querySelector(
+        'input[type="password"]').value;
+      const auth = password === this.state.password;
+      this.setState({
+        authorized: auth
+      });
+    }
+    
+    render() {
+    const login = (
+      <form 
+      className="form"
+      onSubmit={this.authorize}
+      action="#" 
+      >
+      <input className='font' type="password" placeholder="Password"/>
+      <input className='font btn' type="submit"/>
+      </form>
+      );
+
+      const contactInfo = (
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      );
+
+      return (
+        <div className="authorization">
+          <h1>{ this.state.authorized ? 'Contact' : 'Enter the Password'}</h1>
+          {this.state.authorized ? contactInfo : login }
+        </div>
+       
+      );
+   
+}
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contact />
   </React.StrictMode>,
   document.getElementById('root')
 );
